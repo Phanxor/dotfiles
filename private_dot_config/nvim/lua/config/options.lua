9f518eb8d93ff4cdc0ccede330f102fe206e6f6f
Phanxor <jurre.berman@gmail.com>
2024-09-04 08:21:11 +0200

    store latex helper files elsewhere

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -8,8 +8,10 @@ vim.g.UltiSnipsJumpBackwardTrigger = "<S-Tab>"
 vim.g.UltiSnipsSnippetDirectories = { "ultisnips" }
 if vim.loop.os_uname().sysname == "Darwin" then
   vim.g.perl_host_prog = "/Users/gebruiker/.plenv/versions/5.40.0/bin/perl"
+  vim.g.vimtex_view_method = "skim"
 else
   vim.g.perl_host_prog = "/usr/bin/perl"
+  vim.g.vimtex_view_method = "zathura"
 end
 vim.g.python_host_prog = vim.env.HOME .. "/.pyenv/versions/nvim-venv/bin/python"
 vim.g.python3_host_prog = vim.env.HOME .. "/.pyenv/versions/nvim-venv/bin/python3"
@@ -21,17 +23,16 @@ vim.o.autochdir = true
 vim.g.snips_author = "Phanxor"
 vim.g.tex_flavor = "latex"
 vim.g.vimtex_quickfix_open_on_warning = 0
-vim.g.vimtex_view_method = "zathura"
 -- vim.s.pdflatex = "pdflatex -file-line-error -interaction=nonstopmode -halt-on-error -synctex=1"
 --   .. "-output-directory="
 --   .. vim.fn.expand("%:r")
 -- vim.s.latexmk = "latexmk -pdf -output-directory=" .. vim.fn.expand("%:r")
 vim.g.vimtex_compiler_latexmk = {
   aux_dir = function()
-    return vim.fn.expand("%:t:r")
+    return vim.env.HOME .. "/.latex/" .. vim.fn.expand("%:p:h:t")
   end,
-  -- out_dir = function()
-  --   return vim.fn.expand("%:t:r")
-  -- end,
+  out_dir = function()
+    return vim.env.HOME .. "/.latex/" .. vim.fn.expand("%:p:h:t")
+  end,
   options = { "-verbose", "-file-line-error", "-halt-on-error", "-interaction=nonstopmode", "-synctex=1" },
 }
